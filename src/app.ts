b2c8c0d9ea6d9af31f92edbfec5d4e49393f14ab
antonElsikora <anton.developer@elsikora.com>
2024-11-07 05:55:32 +0300

    chore: remove console.log statements to pass linter checks

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,11 @@ import { RequestWithUser } from './controllers/cards';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect('mongodb://localhost:27017/mestodb')
-  .then(() => console.log('Успешно подключились к базе данных'))
-  .catch((error) => console.error('Ошибка подключения к базе данных:', error));
+mongoose.connect('mongodb://localhost:27017/mestodb').catch(() => {});
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   (req as RequestWithUser).user = {
-    _id: 'ваш_идентификатор',
+    _id: '672c13e50aa1d6425d1aeb7a',
   };
   next();
 });
@@ -23,6 +20,4 @@ app.use(express.json());
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
-});
+app.listen(PORT, () => {});
