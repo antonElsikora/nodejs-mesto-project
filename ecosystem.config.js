52b1d9f392c776a5734ad064e9fd5370803bca85
antonElsikora <anton.developer@elsikora.com>
2025-03-30 01:32:05 +0300

    FIX: rebuild ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: '.env.deploy' });
+
 module.exports = {
   apps: [
     {
@@ -10,13 +12,13 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: 'anton',
-      host: '158.160.163.154',
-      ref: 'origin/main',
-      repo: 'git@github.com:antonElsikora/nodejs-mesto-project.git',
-      path: '/home/anton/production',
+      user: process.env.DEPLOY_USER,
+      host: process.env.DEPLOY_HOST,
+      ref: process.env.DEPLOY_REF,
+      repo: process.env.DEPLOY_REPO,
+      path: process.env.DEPLOY_PATH,
       'pre-deploy-local':
-        'scp .env anton@158.160.163.154:/home/anton/production/current/',
+        `scp .env ${process.env.DEPLOY_USER}@${process.env.DEPLOY_HOST}:${process.env.DEPLOY_PATH}/current/`,
       'post-deploy':
         'npm install && npm run build && pm2 restart ecosystem.config.js --env production',
     },
